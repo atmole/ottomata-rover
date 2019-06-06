089dd93b7562e3357ee2ca7173ab0f2663019ecd
Peter Radi <pradi@Peter-iMac.local>
2019-06-06 21:43:29 +0200

    updated motor test

--- moto-test.py
+++ moto-test.py
@@ -10,19 +10,37 @@ import gpiozero
 # gpiozero.DigitalOutputDevice(pin, *, active_high=True,
 # initial_value=False, pin_factory=None)
 
-servotime = 1
-steptime = 0.1
+servotime = 0.5
+steptime = 0.01
 
-servo = gpiozero.Servo(14)
+servo = gpiozero.Servo(23)
 
-en = gpiozero.DigitalOutputDevice(5, active_high=False, initial_value=True)
+en1 = gpiozero.DigitalOutputDevice(0, initial_value=False)
+en2 = gpiozero.DigitalOutputDevice(5, initial_value=False)
 
-l1 = gpiozero.DigitalOutputDevice(6, active_high=False, initial_value=True)
-l2 = gpiozero.DigitalOutputDevice(13, active_high=False, initial_value=True)
-l3 = gpiozero.DigitalOutputDevice(19, active_high=False, initial_value=True)
-l4 = gpiozero.DigitalOutputDevice(26, active_high=False, initial_value=True)
+l1 = gpiozero.DigitalOutputDevice(6)
+l2 = gpiozero.DigitalOutputDevice(13)
+l3 = gpiozero.DigitalOutputDevice(19)
+l4 = gpiozero.DigitalOutputDevice(26)
 
-print("initialized ports, start servo test...")
+
+def stepper_function(stepcount):
+    for i in range(1, stepcount):
+        l1.on()
+        sleep(steptime)
+        l1.off()
+        l2.on()
+        sleep(steptime)
+        l2.off()
+        l3.on()
+        sleep(steptime)
+        l3.off()
+        l4.on()
+        sleep(steptime)
+        l4.off()
+
+
+print("Initialized ports, starting servo test...")
 
 for i in range(1, 3):
     sleep(servotime)
@@ -35,22 +53,17 @@ for i in range(1, 3):
 
 servo.detach()
 
-print("servo test done, stepper test...")
+print("Servo test done. Starting stepper test 1: Nothing is enabled")
+stepper_function(100)
+print("Starting stepper test 2: Motor 1 enabled")
+en1.on()
+stepper_function(200)
+print("Starting stepper test 3: Motor 2 enabled")
+en1.off()
+en2.on()
+stepper_function(200)
+print("Starting stepper test 4: Both motor enabled")
+en1.on()
+stepper_function(200)
 
-en.on()
-
-for i in range(1, 10):
-    l1.on()
-    sleep(steptime)
-    l1.off()
-    l2.on()
-    sleep(steptime)
-    l2.off()
-    l3.on()
-    sleep(steptime)
-    l3.off()
-    l4.on()
-    sleep(steptime)
-    l4.off()
-
-print("stepper test done, exit...")
+print("Stepper tests done, exiting...")
